3fb75a0b86b74f7211119fdb2712bb5b63e27a08
Ranjith Rayaprolu <ranjithpavan@gmail.com>
2020-07-01 15:18:51 -0600

    Used Click to organize the script

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ Repository
 **This repository is for automating AWS using Python**
 
 ### 01- Static
-This is a script that will sync a local directory to an s3 bucket, and can configure Route53 and cloudfront. 
+This is a script that will sync a local directory to an s3 bucket, and can configure Route53 and cloudfront.
+
+#### Features
+It currently has the following Features.
+
+- list-buckets
+- list-bucket-objects
---
+++ S3-Static-Website/s3.py
@@ -0,0 +1,26 @@
+import boto3
+import click
+
+session = boto3.Session(profile_name='automation')
+s3 = session.resource('s3')
+
+@click.group()
+def cli():
+    "Deployes websites to AWS"
+    pass
+
+@cli.command('list-buckets')
+def list_buckets():
+    "List all S3 buckets"
+    for bucket in s3.buckets.all():
+        print(bucket)
+
+@cli.command('list-bucket-objects')
+@click.argument('bucket')
+def list_bucket_objects(bucket):
+    "List objects in a given bucket"
+    for object in s3.Bucket(bucket).objects.all():
+        print(object)
+
+if __name__== '__main__':
+    cli()
