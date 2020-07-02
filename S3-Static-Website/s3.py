b80230df1c184b98888653c2afe08b276898ddf5
Ranjith Rayaprolu <ranjithpavan@gmail.com>
2020-07-02 15:17:59 -0600

    corrected related to error

--- S3-Static-Website/s3.py
+++ S3-Static-Website/s3.py
@@ -93,7 +93,7 @@ def sync(pathname, bucket):
     def handle_directory(target):
         for p in target.iterdir():
             if p.is_dir(): handle_directory(p)
-            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(p)))
+            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
 
     handle_directory(root)
 
